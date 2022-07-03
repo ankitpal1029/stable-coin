06597294d2b3c8bab92d8cb1094ed816c1263b79
ankitpal1029 <ankitkumarpal27@gmail.com>
2022-07-04 00:00:25 +0530

    refactoring with dev env

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,15 @@
 import abi from "./TransactionsABI.json";
-import lenderDeployments from "../../hardhat/deployments/localhost/Lender.json";
+import lenderLocalDeployments from "../../hardhat/deployments/localhost/Lender.json";
+import lenderRinkebyDeployments from "../../hardhat/deployments/rinkeby/Lender.json";
 
 export const contractABI = abi.abi;
 export const contractAddress = "0xFe04DFB66af39B035293bd002c10229c6E6c010b";
 
-export const lenderContractABI = lenderDeployments.abi;
-export const lenderContractAddrsss = lenderDeployments.address;
+export const lenderContractABI =
+  process.env.NODE_ENV === "development"
+    ? lenderLocalDeployments.abi
+    : lenderRinkebyDeployments.abi;
+export const lenderContractAddrsss =
+  process.env.NODE_ENV === "development"
+    ? lenderLocalDeployments.address
+    : lenderRinkebyDeployments.address;
